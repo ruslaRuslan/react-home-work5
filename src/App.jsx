154724cf1bd6e52005b71e4c1715214e7cb4015e
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-04 11:12:07 +0400

    Users ve Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import Counter from "./components/Counter"
-import Index from "./components/Index"
+import Index from "./components/Users"
 
 function App() {
 
 
   return (
     <> 
-    
+    <Users/>
     
     </>
   )
 }
 
-export default App
+export default App;
--- src/components/Index/index.jsx
+++
@@ -1,4 +0,0 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-const url = 'https://restcountries.com/v3.1/name/azerbaijan'
-
