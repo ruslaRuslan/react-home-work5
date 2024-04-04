04c874342d04376b9fcf8546c03ec0aeb070c707
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-04 12:15:38 +0400

    is tamam

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,15 +1,14 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-const url = `https://restcountries.com/v3.1/name/${countryName}`
 
 const Users = () => {
-    const [contryData, setCountryData] = useState(null)
+    const [countryData, setCountryData] = useState(null)
     const [countryName, setCountryName] = useState("")
     const [error, setError] = useState(null)
 
     const fetchData = async () => {
         try {
-            const response = await axios.get(url)
+            const response = await axios.get(`https://restcountries.com/v3.1/name/${countryName}`)
             setCountryData(response.data[0])
         } catch (error) {
             setError(error.message)
@@ -20,11 +19,26 @@ const Users = () => {
         if (countryName) {
             fetchData()
         }
-    }, [contryData]);
+    }, [countryName]);
+
+    const handleInputChange = (event) => {
+        setCountryName(event.target.value)
+    }
 
     return (
         <>
-
+            <input type="text" placeholder="country name" value={countryName} onChange={handleInputChange} />
+            {error && <p>Error:{error}</p>}
+            {countryData && (
+                <div>
+                    <h2>{countryData.name.common}</h2>
+                    <p>Capital: {countryData.capital}</p>
+                    <p>Population: {countryData.population}</p>
+                    <p>Region: {countryData.region}</p>
+                    {/*basqa bolgeleri bura yerlesdire bilersiz*/}
+                </div>
+            )
+            }
 
         </>
     )
